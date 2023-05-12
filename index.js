bab46851c2496320bd19575bad3e0b1f33c9bc97
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-12 19:07:50 +0600

    confirm using pactch and vercel json added

--- index.js
+++ index.js
@@ -63,6 +63,20 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+        $set: {
+          status: updatedBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
