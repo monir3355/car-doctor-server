59bba793b82e5b4bfd0fbb89b2b8e1b3a215892d
monir3355 <16173103036@cse.bubt.edu.bd>
2023-05-12 16:54:49 +0600

    booking post and booking read and my booking show by email

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
       const result = await serviceCollection.findOne(query);
       res.send(result);
     });
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
